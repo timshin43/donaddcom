14eb9667f09c039dd04c61f3fc4ed98d5508ff91
timshin43 <timshin.sergei@gmail.com>
2019-04-09 21:21:49 -0700

    statis root chnaged

--- anim_website/settings.py
+++ anim_website/settings.py
@@ -177,7 +177,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
+# STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 
