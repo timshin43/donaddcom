ce5a4b8770bceacb5fcee637f3d5773dfdc69fe8
timshin43 <timshin.sergei@gmail.com>
2019-04-09 20:12:13 -0700

    added whitenoise to middlear, upated requirements

--- anim_website/settings.py
+++ anim_website/settings.py
@@ -77,6 +77,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+	'whitenoise.middleware.WhiteNoiseMiddleware',
 	'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
