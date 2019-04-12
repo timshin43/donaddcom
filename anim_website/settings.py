a82187a18d2b5522d18b9707085a1e54a1142ed2
timshin43 <timshin.sergei@gmail.com>
2019-04-11 20:57:53 -0700

    whitehoise placed upper in middleware

--- anim_website/settings.py
+++ anim_website/settings.py
@@ -76,8 +76,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
 	'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
 	'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
