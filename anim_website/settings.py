687511da0121e7065afe037da32073167d453ae3
timshin43 <timshin.sergei@gmail.com>
2019-04-14 14:36:55 -0700

    settings

--- anim_website/settings.py
+++ anim_website/settings.py
@@ -72,7 +72,6 @@ INSTALLED_APPS = [
     'blog',
 	'storages',
 	
-	
 
 ]
 
@@ -188,12 +187,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # MEDIA_URL = config('MEDIA_URL')
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", "") 
-AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "")
-AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME", "")
+AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID", "") 
+AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY", "")
+AWS_STORAGE_BUCKET_NAME = config("AWS_STORAGE_BUCKET_NAME", "")
 AWS_QUERYSTRING_AUTH = False 
-AWS_S3_CUSTOM_DOMAIN = os.environ.get("AWS_S3_CUSTOM_DOMAIN", "")
-MEDIA_ROOT = os.environ.get("MEDIA_URL", "")
+AWS_S3_CUSTOM_DOMAIN = config("AWS_S3_CUSTOM_DOMAIN", "")
+MEDIA_ROOT = config("MEDIA_URL", "")
 MEDIA_URL = '/media/'
 
 
