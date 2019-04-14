63f05515e7f3519a3df392220844ffe3a42559ff
timshin43 <timshin.sergei@gmail.com>
2019-04-14 13:46:40 -0700

    S3 settings changed

--- anim_website/settings.py
+++ anim_website/settings.py
@@ -181,9 +181,20 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
+#MEDIA SETTINGS
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = config('MEDIA_URL')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = config('MEDIA_URL')
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID", "") 
+AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY", "")
+AWS_STORAGE_BUCKET_NAME = config("AWS_STORAGE_BUCKET_NAME", "")
+AWS_QUERYSTRING_AUTH = False 
+AWS_S3_CUSTOM_DOMAIN = config("AWS_S3_CUSTOM_DOMAIN", "")
+MEDIA_ROOT = config("MEDIA_URL", "")
+MEDIA_URL = '/media/'
+
+
 
 ####################################
 ##  CKEDITOR CONFIGURATION ##
