bd3e7d1d95a618b936b0ba5a35ec15d9f8fe8c72
timshin43 <timshin.sergei@gmail.com>
2019-04-14 14:39:20 -0700

    g

--- anim_website/settings.py
+++ anim_website/settings.py
@@ -70,7 +70,6 @@ INSTALLED_APPS = [
     'django_countries',
     'funny_stories',
     'blog',
-	'storages',
 	
 
 ]
@@ -183,17 +182,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 #MEDIA SETTINGS
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# MEDIA_URL = config('MEDIA_URL')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = config('MEDIA_URL')
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID", "") 
-AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY", "")
-AWS_STORAGE_BUCKET_NAME = config("AWS_STORAGE_BUCKET_NAME", "")
-AWS_QUERYSTRING_AUTH = False 
-AWS_S3_CUSTOM_DOMAIN = config("AWS_S3_CUSTOM_DOMAIN", "")
-MEDIA_ROOT = config("MEDIA_URL", "")
-MEDIA_URL = '/media/'
+
 
 
 
