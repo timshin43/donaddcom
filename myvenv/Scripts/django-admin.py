ed5a769cb7369a76c455027863e7d8256f9b39f3
timshin43 <timshin.sergei@gmail.com>
2019-04-18 22:24:36 -0700

    deleted redundant venv from the root, updated password change templ

--- myvenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\timshin\desktop\my stuff\animal website\anim_website\myvenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
