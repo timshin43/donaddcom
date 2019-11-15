6f964a9f85732d81b840a0f453975d5945d52769
timshin43 <timshin.sergei@gmail.com>
2019-11-15 19:22:53 +0100

    new effort

--- anim_website/settings.py
+++ anim_website/settings.py
@@ -47,7 +47,8 @@ SECRET_KEY = config('SECRET_KEY')
 # DEBUG = True
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
+#ALLOWED_HOSTS = ['127.0.0.1', '.localhost']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
 
 # Application definition
 
